142f6bf26df7c3372330e5da69691969861c41fe
unknown <gross3000@yandex.ru>
2026-01-27 02:51:34 +0300

    feat(app users): add model payment

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework.serializers import ModelSerializer
 
-from users.models import User
+from users.models import Payment, User
 
 
 class UserSerializer(ModelSerializer):
+    """Сериализатор по пользователям"""
+
     class Meta:
         model = User
+        fields = "__all__"
+
+
+class PaymentSerializer(ModelSerializer):
+    """Сериализатор по платежам"""
+
+    class Meta:
+        model = Payment
         fields = "__all__"
