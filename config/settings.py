d486ebff3f092b876b683be9f0ff1f96159b7760
unknown <gross3000@yandex.ru>
2026-03-03 22:19:24 +0300

    feat: add docker-compose.yml, Dockerfile

--- config/settings.py
+++ config/settings.py
@@ -89,11 +89,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("NAME_BD"),
-        "USER": os.getenv("USER_BD"),
-        "PASSWORD": os.getenv("PASSWORD_BD"),
-        "HOST": os.getenv("HOST_BD"),
-        "PORT": os.getenv("PORT_BD"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
