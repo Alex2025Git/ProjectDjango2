7a0967c71a68ed0969830abf5a08813c0db8ca99
unknown <gross3000@yandex.ru>
2026-03-04 23:59:55 +0300

    refactor: settings.py

--- config/settings.py
+++ config/settings.py
@@ -133,7 +133,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = (BASE_DIR / "static",)
 STATIC_ROOT = BASE_DIR / "static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
